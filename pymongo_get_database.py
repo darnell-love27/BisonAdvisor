b0a8c9094b9bd75d92817a134c2de4d576f3eab2
Darnell Love <101828196+darnell-love27@users.noreply.github.com>
2023-12-09 03:23:59 -0500

    Update pymongo_get_database.py

--- pymongo_get_database.py
+++ pymongo_get_database.py
@@ -1,1 +1,16 @@
+from pymongo import MongoClient
 
+def get_database():
+    # Replace the connection string with your MongoDB Atlas connection string
+    CONNECTION_STRING = "mongodb+srv://<username>:<password>@cluster0.gplhetr.mongodb.net/?retryWrites=true&w=majority"
+
+    try:
+        # Create a MongoClient using the provided connection string
+        client = MongoClient(CONNECTION_STRING)
+        # Access your specific database
+        db = client['BisonAdvisor']
+        return db
+    except Exception as e:
+        # Handle connection errors
+        print(f"Connection to MongoDB failed: {e}")
+        return None
