52c027db3e609dc8565cfebf95f4d9bd2075092d
Darnell Love <101828196+darnell-love27@users.noreply.github.com>
2023-12-09 03:21:21 -0500

    Update StudentInput.py
    Implemented changes to the code so that the data being pushed to the JSON File can also be pushed to MongoDB once the connection is successful.

--- Databases/StudentInput.py
+++ Databases/StudentInput.py
@@ -1,72 +1,82 @@
 import json
 import os
+from pymongo import MongoClient
+from pymongo_get_database import get_database
 
-class Student: #Student Creator
-    def __init__(self, name = " ", classification = " ", major = " ", studentID = 0, prev_courses=[]):
-        #Initialized for student characteristics
+
+class Student:
+    def __init__(self, name="", classification="", major="", studentID=0, courses=None):
+        # Initialize student characteristics
         self.name = name
         self.studentID = studentID
         self.major = major
         self.classification = classification
-        self.prev_courses = prev_courses
+        self.courses = courses if courses else []
 
     @classmethod
-    def from_dict(source):
-        #Class method that creates a Student object from a dictionary
-        return Student(
+    def from_dict(cls, source):
+        # Class method that creates a Student object from a dictionary
+        return cls(
             source.get('name', ''),
             source.get('classification', ''),
             source.get('major', ''),
             source.get('studentID', 0),
-            source.get('Previous Courses', [])
+            source.get('courses', [])
         )
 
     def to_dict(self):
-        # Method to convert a Student object into a dictionary 
+        # Method to convert a Student object into a dictionary
         return {
             'name': self.name,
             'studentID': self.studentID,
             'major': self.major,
             'classification': self.classification,
-            'Previous Courses': self.prev_courses
-
+            'courses': self.courses
         }
 
     def add_values_to_keys(self):
-        # Method to take user input
-        # Set Student object attributes
+    # Method to take user input and set Student object attributes
         self.name = input("Enter student name: ")
         self.studentID = input("Enter student ID: ")
         self.major = input("Enter student major: ")
         self.classification = input("Enter student classification: ")
-        course_input = input("Enter courses taken separated by commas: ")
-        self.prev_courses = course_input.split(',')
+        courses_input = input("Enter courses taken separated by commas (course_code,course_name): ")
+        self.courses = []
+        for course_input in courses_input.split(','):
+            course_data = course_input.split(',')
+            if len(course_data) == 2:
+                self.courses.append({
+                    'course_code': course_data[0].strip(),
+                    'course_name': course_data[1].strip()
+                })
+            else:
+                print(f"Invalid input: {course_input}. Expected format: course_code, course_name")
+
 
     def __repr__(self):
         # String representation of Student Object
         return f"Student(\
-                name={self.name}, \
-                studentID={self.studentID}, \
-                major={self.major}, \
-                classification={self.classification}, \
-                Previous Courses={self.prev_courses}\
-            )"
+            name={self.name}, \
+            studentID={self.studentID}, \
+            major={self.major}, \
+            classification={self.classification}, \
+            courses={self.courses}\
+        )"
 
 # Function that reads existing JSON data from a file
 def read_existing_data(file_name):
     existing_data = []
-    #Checks if file exists and non-empty
-    if os.path.exists("StudentDatabase.json") and os.path.getsize("StudentDatabase.json") > 0:
+    # Checks if file exists and non-empty
+    if os.path.exists(file_name) and os.path.getsize(file_name) > 0:
         try:
             # Load existing JSON data from the file
-            with open("StudentDatabase.json", 'r') as file:
+            with open(file_name, 'r') as file:
                 existing_data = json.load(file)
         except json.JSONDecodeError as e:
             # Handles JSON decoding error if the file doesn't contain valid JSON data
             print(f"Error reading JSON data: {e}")
             return existing_data
     return existing_data           
-
 
 # Adds new student data to the existing JSON file
 def append_student_data(file_name):
@@ -90,7 +100,18 @@ def append_student_data(file_name):
         if choice.lower() != 'yes':
             break
 
+def pushToMongoDB():
+    file_name = "StudentDatabase.json"
+
+    with open(file_name, 'r') as file:
+        existing_data = json.load(file)
+
+    dbname = get_database()
+    collection_name = dbname["StudentData"]
+    collection_name.insert_many(existing_data)
+
 if __name__ == "__main__":
     file_name = "StudentDatabase.json"
     append_student_data(file_name)
     print(f"Student data has been appended to '{file_name}' as a JSON file.")
+    pushToMongoDB()
